8126b19ac1b0ab95e4e5cf5763e76eea27354595
Dzedz Timofei <dzedz@internet.ru>
2026-08-04 14:23:04 +0300

    Added register and logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function register(Request $request): Redirector|RedirectResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'Registration successful!');
+    }
+
     public function login(Request $request): RedirectResponse
     {
         // Validating inputs
@@ -25,5 +48,16 @@ class UserController extends Controller
 
         // if login failed
         return back()->withErrors(['email' => 'The provided credentials do not match our records.'])->onlyInput('email');
+    }
+
+    public function logout(Request $request): Redirector|RedirectResponse
+    {
+        Auth::logout();
+
+        // Invalidate session
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('success', 'You are now logged out!');
     }
 }
