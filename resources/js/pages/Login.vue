f37c0622615d31a0e3885f8b0c2afbc7c81c82f3
Dzedz Timofei <dzedz@internet.ru>
2026-08-04 20:22:59 +0300

    Forms and transitions

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+    import { Form } from '@inertiajs/vue3';
+    import { Link } from '@inertiajs/vue3'
     import { ref } from 'vue';
 
     const inputValue = ref({
@@ -10,14 +12,13 @@
 </script>
 
 <template>
-    <p>{{ inputValue }}</p>
         <div class="flex flex-1 flex-col items-center mt-10 sm:mx-auto sm:w-full sm:max-w-sm min-h-full px-6 py-12 lg:px-8">
             <h1 class="text-5xl">Sign in</h1>
-            <form method="post" action="/login" id="login-form" class="flex flex-1 flex-col space-y-6 p-4">
-                    <input v-model="inputValue.login" placeholder="E-Mail" type="text" name="username" required autocomplete="on" class="rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
+            <Form method="post" action="/api/login" id="login-form" class="flex flex-1 flex-col space-y-6 p-4">
+                    <input v-model="inputValue.login" placeholder="E-Mail" type="text" name="email" required autocomplete="on" class="rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
                     <input v-model="inputValue.password" placeholder="Password" type="password" name="password" required autocomplete="off" class="rounded-[3px] px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
                     <button type="submit" class="cursor-pointer rounded-[3px] px-6 py-5 text-2xl text-black bg-[#a8d06f]"><span>Continue </span><i class="pi pi-sign-in"></i></button>
-            </form>
-            <p>Not registered yet? <a href="/Register" class="hover:text-[#d7e5d2]">Registration</a></p>
+            </Form>
+            <p>Not registered yet? <Link href="/register" view-transition class="hover:text-[#d7e5d2]">Registration</Link></p>
         </div>
 </template>
