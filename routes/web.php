e44d3d683a2e2812fa0e525d05bd49fa6288ede4
Dzedz Timofei <dzedz@internet.ru>
2026-08-04 14:22:46 +0300

    Added routes for user auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::inertia('/', 'Login')->name('login');
-Route::inertia('/', 'Registration')->name('registration');
+Route::inertia('/login', 'Login')->middleware('guest')->name('login');
+Route::post('login', [UserController::class, 'login'])->middleware('guest');
+
+Route::inertia('/register', 'Register')->name('registration');
+Route::post('register', [UserController::class, 'register'])->middleware('guest');
+
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth')->name('logout');
