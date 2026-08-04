d9318ea79dced674a0d76d162e72793ef0f73667
Dzedz Timofei <dzedz@internet.ru>
2026-08-04 20:44:15 +0300

    Created components

---
+++ resources/js/components/ChatComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div id="chat-window" class="flex flex-2 flex-col">
+
+    </div>
+</template>
+
+<style scoped></style>
---
+++ resources/js/components/SideMenuComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div id="side-menu" class="flex flex-col">
+
+    </div>
+</template>
+
+<style scoped></style>
