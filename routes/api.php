bd1cd1461e2e8149f1f813234415d6ef24daffcb
Dzedz Timofei <dzedz@internet.ru>
2026-08-04 19:19:34 +0300

    Moved api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [UserController::class, 'register'])->middleware('guest:sanctum');
+Route::post('/logout', function (Request $request) {
+    return $request->user()->currentAccessToken()->delete();
+})->middleware('auth:sanctum')->name('logout');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest:sanctum');
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::inertia('/login', 'Login')->middleware('guest')->name('login');
-Route::post('login', [UserController::class, 'login'])->middleware('guest');
-
-Route::inertia('/register', 'Register')->name('registration');
-Route::post('register', [UserController::class, 'register'])->middleware('guest');
-
-Route::post('logout', [UserController::class, 'logout'])->middleware('auth')->name('logout');
+Route::inertia('/register', 'Register')->middleware('guest')->name('register');
