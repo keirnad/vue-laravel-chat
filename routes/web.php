1b51a85d88444e68bc982b2824d6f31a3839f1c3
Dzedz Timofei <dzedz@internet.ru>
2026-08-04 20:30:39 +0300

    Redirect if not registered

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['middleware' => ['auth']], function() {
+    Route::inertia('/', 'Home')->name('home');
+});
 Route::inertia('/login', 'Login')->middleware('guest')->name('login');
 Route::inertia('/register', 'Register')->middleware('guest')->name('register');
