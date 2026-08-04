3d511c98c63a023a1f4d958e276f883666d4dd64
Dzedz Timofei <dzedz@internet.ru>
2026-08-04 12:58:29 +0300

    Add route

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Welcome')->name('home');
+Route::inertia('/', 'Login')->name('login');
+Route::inertia('/', 'Registration')->name('registration');
