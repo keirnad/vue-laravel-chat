9340c74bf0576b3c099c8f24f928fb5a08f9f6fb
Dzedz Timofei <dzedz@internet.ru>
2026-08-05 21:59:41 +0300

    Added responsiveness

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     import { Form } from '@inertiajs/vue3';
-    import { Link } from '@inertiajs/vue3'
-    import { Head } from '@inertiajs/vue3'
+    import { Link } from '@inertiajs/vue3';
+    import { Head } from '@inertiajs/vue3';
     import { ref } from 'vue';
 
     const inputValue = ref({
@@ -17,13 +17,15 @@
         <title>Login - YChat</title>
         <meta name="description" content="login page of YChat">
     </Head>
-        <div class="flex flex-1 flex-col items-center mt-10 sm:mx-auto sm:w-full sm:max-w-sm min-h-full px-6 py-12 lg:px-8">
-            <h1 class="text-5xl">Sign in</h1>
-            <Form method="post" action="/api/login" id="login-form" class="flex flex-1 flex-col space-y-6 p-4">
-                    <input v-model="inputValue.login" placeholder="E-Mail" type="text" name="email" required autocomplete="on" class="rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
-                    <input v-model="inputValue.password" placeholder="Password" type="password" name="password" required autocomplete="off" class="rounded-[3px] px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
-                    <button type="submit" class="cursor-pointer rounded-[3px] px-6 py-5 text-2xl text-black bg-[#a8d06f]"><span>Continue </span><i class="pi pi-sign-in"></i></button>
-            </Form>
-            <p>Not registered yet? <Link href="/register" view-transition class="hover:text-[#d7e5d2]">Registration</Link></p>
+        <div class="flex flex-col justify-center mt-10 min-h-full px-6 py-12 lg:px-8">
+            <h1 class="text-5xl self-center">Sign in</h1>
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <Form method="post" action="/login" id="login-form" class="space-y-6 p-4">
+                    <input v-model="inputValue.login" placeholder="E-Mail" type="text" name="email" required autocomplete="on" class="block w-full rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
+                    <input v-model="inputValue.password" placeholder="Password" type="password" name="password" required autocomplete="off" class="block w-full rounded-[3px] px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
+                    <button type="submit" class="block w-full cursor-pointer rounded-[3px] px-6 py-5 text-2xl text-black bg-[#a8d06f] hover:bg-[#8db05b]"><span>Continue </span><i class="pi pi-sign-in"></i></button>
+                </Form>
+            </div>
+            <p class="self-center">Not registered yet? <Link href="/register" view-transition class="hover:text-[#d7e5d2]">Registration</Link></p>
         </div>
 </template>
--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     import { Form } from '@inertiajs/vue3';
-    import { Link } from '@inertiajs/vue3'
-    import { Head } from '@inertiajs/vue3'
+    import { Link } from '@inertiajs/vue3';
+    import { Head } from '@inertiajs/vue3';
     import { ref } from 'vue';
 
     const inputValue = ref({
@@ -17,14 +17,16 @@
         <title>Register - YChat</title>
         <meta name="description" content="Registration page of YChat">
     </Head>
-        <div class="flex flex-1 flex-col items-center mt-10 sm:mx-auto sm:w-full sm:max-w-sm min-h-full px-6 py-12 lg:px-8">
-            <h1 class="text-5xl">Sign up</h1>
-            <Form method="post" action="/api/register" id="login-form" class="flex flex-1 flex-col space-y-6 p-4">
-                    <input v-model="inputValue.email" placeholder="E-Mail" type="text" name="email" required autocomplete="on" class="rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
-                    <input v-model="inputValue.name" placeholder="Username" type="text" name="name" required autocomplete="on" class="rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
-                    <input v-model="inputValue.password" placeholder="Password" type="password" name="password" required autocomplete="off" class="rounded-[3px] px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
-                    <button type="submit" class="cursor-pointer rounded-[3px] px-6 py-5 text-2xl text-black bg-[#a8d06f]"><span>Continue </span><i class="pi pi-sign-in"></i></button>
-            </Form>
-            <p>Already have an account? <Link href="/login" view-transition class="hover:text-[#d7e5d2]">Login</Link></p>
+        <div class="flex flex-col justify-center mt-10 min-h-full px-6 py-12 lg:px-8">
+            <h1 class="text-5xl self-center">Sign up</h1>
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <Form method="post" action="/register" id="login-form" class="flex flex-1 flex-col space-y-6 p-4">
+                    <input v-model="inputValue.email" placeholder="E-Mail" type="text" name="email" required autocomplete="on" class="block w-full rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
+                    <input v-model="inputValue.name" placeholder="Username" type="text" name="name" required autocomplete="on" class="block w-full rounded-[3px] text px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
+                    <input v-model="inputValue.password" placeholder="Password" type="password" name="password" required autocomplete="off" class="block w-full rounded-[3px] px-6 py-5 text-2xl outline-0 text-white bg-[#94a8af] border-2 border-[#94a8af] focus:border-[#a8d06f]">
+                    <button type="submit" class="cursor-pointer rounded-[3px] px-6 py-5 text-2xl text-black bg-[#a8d06f] hover:bg-[#8db05b]"><span>Continue </span><i class="pi pi-sign-in"></i></button>
+                </Form>
+            </div>
+            <p class="self-center">Already have an account? <Link href="/login" view-transition class="hover:text-[#d7e5d2]">Login</Link></p>
         </div>
 </template>
